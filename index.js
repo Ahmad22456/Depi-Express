be03b1edf6e07ee196bd99c52e730c63f6e2760b
Ahmad Fawzy <shadow0181456@gmail.com>
2024-08-19 19:23:33 +0300

    Get Request

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// Get Request
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "Get Request",
+  });
+});
