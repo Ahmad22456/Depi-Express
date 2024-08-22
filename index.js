98f87ddf543973948a5079502308b0ec98366b30
Ahmad Fawzy <shadow0181456@gmail.com>
2024-08-22 13:22:36 +0300

    Added query

--- index.js
+++ index.js
@@ -5,92 +5,40 @@ const fs = require("fs");
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-// Middleware
+//Middleware
 app.use(express.json());
 
-// Get Request
+//Routes
 app.get("/", (req, res) => {
   fs.readFile("database.json", "utf-8", (err, data) => {
-    if (err) res.status(400).json(err);
-    if (data) res.status(200).json({ Msg: JSON.parse(data) });
-  });
-});
-
-// Post Request
-app.post("/", (req, res) => {
-  fs.readFile("database.json", "utf-8", (err, data) => {
-    if (err) res.status(400).json(err);
+    if (err) res.status(400).json({ Error: err });
     if (data) {
+      const category = req.query.category;
+      const maxPrice = req.query.maxPrice;
       const parsedData = JSON.parse(data);
-      const id = parsedData["Last_Id"] + 1;
-      const Users = parsedData["Users"];
-      const { name } = req.body;
-      Users.push({ id, name });
-
-      fs.writeFile(
-        "database.json",
-        JSON.stringify({ Users, Last_Id: id }),
-        (err) => {
-          if (err) {
-            res.status(400).json(err);
-          } else {
-            res.status(200).json({ Msg: "User has been added" });
-          }
-        }
-      );
-    }
-  });
-});
-
-// Put Request
-app.put("/:id", (req, res) => {
-  fs.readFile("database.json", "utf-8", (err, data) => {
-    if (err) res.status(400).json({ err });
-    if (data) {
-      const parsedData = JSON.parse(data);
-      const id = req.params.id;
-      const { name } = req.body;
-      const Users = parsedData["Users"];
-      Users[Users.findIndex((e) => e.id == id)].name = name;
-
-      fs.writeFile(
-        "database.json",
-        JSON.stringify({ Users, Last_Id: parsedData["Last_Id"] }),
-        (err) => {
-          if (err) {
-            res.status(400).json({ err });
-          } else {
-            res.status(200).json({ Msg: "User has been updated" });
-          }
-        }
-      );
-    }
-  });
-});
-
-// Delete Request
-app.delete("/:id", (req, res) => {
-  fs.readFile("database.json", "utf-8", (err, data) => {
-    if (err) res.status(400).json({ err });
-    if (data) {
-      const parsedData = JSON.parse(data);
-      const id = req.params.id;
-      let Users = parsedData["Users"];
-      Users = Users.filter((e) => e.id != id);
-
-      fs.writeFile(
-        "database.json",
-        JSON.stringify({ Users, Last_Id: parsedData["Last_Id"] }),
-        (err) => {
-          if (err) {
-            res.status(400).json({ err });
-          } else {
-            res.status(200).json({ Msg: "User has been deleted" });
-          }
-        }
-      );
+      if (!category && !maxPrice) {
+        res.status(200).json(parsedData);
+      } else if (category && !maxPrice) {
+        res.status(200).json(parsedData.filter((x) => x.category == category));
+      } else if (!category && maxPrice) {
+        res
+          .status(200)
+          .json(parsedData.filter((x) => x.price <= parseFloat(maxPrice)));
+      } else if (category && maxPrice) {
+        res
+          .status(200)
+          .json(
+            parsedData.filter(
+              (x) => x.category == category && x.price <= parseFloat(maxPrice)
+            )
+          );
+      } else {
+        res.status(200).json({ Msg: "Invalid request" });
+      }
     }
   });
 });
